22f66f5c3c30c2a7043e38bcb44caf828c20ccd9
Ahmad Mardiana <amdtkj@gmail.com>
2024-02-13 22:22:32 +0700

    Image to img

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,3 @@
-import Image from "next/image"
-import { Navbar } from "./NavBar"
 import utilStyles from '@styles/utils.module.css';
 import Link from "next/link";
 
@@ -7,7 +5,7 @@ export const Header = () => {
     return (
         <div className="top-10 flex flex-col mx-auto">
             <Link href="/" className="mx-auto pt-10">
-              <Image
+              <img
                 priority
                 src="/assets/images/profile.jpg"
                 className={utilStyles.borderCircle}
@@ -19,7 +17,7 @@ export const Header = () => {
             <h1 className=" mx-auto font-extralight text-xl pb-4">4 Years Experienced Backend Developer</h1>          
             <div className=" flex flow-row mx-auto">
             <a href="#">
-              <Image
+              <img
                   className="mx-2 hover:placeholder:Test"
                   priority
                   src="/assets/icons/linkedin-logo.png"
@@ -28,7 +26,7 @@ export const Header = () => {
                 />
             </a>
             <a>
-            <Image
+            <img
                 priority
                 src="/assets/icons/github-logo.png"
                 height={25}
